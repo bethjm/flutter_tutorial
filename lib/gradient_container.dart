2509dd8e27ce37a06b2a53809cbac9c43cc8cc5a
bethjm <b.jeanne.mills@gmail.com>
2024-10-14 14:30:35 -0700

    added assets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,23 +3,26 @@ import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
 
+final startAlignment = Alignment.topLeft;
+final endAlignment = Alignment.bottomRight;
+
   const GradientContainer({super.key});
     //do this to forward key argument GradientContainer({key}): super(key:key);
     //can accept named arguments? a, b could be arguments in {} ex. GradientContainer() {a,b}
   
     @override
-	Widget build(context) {
+	Widget build(BuildContext context) {
     return Container (
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.deepPurple, Colors.blue],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
           ), //LinearGradient
         ), // BoxDecoration
       // says where to put the code in the scaffolding
         child: Center(
-            child: StyledText()
+            child: StyledText("hello world..")
         //says what will show
         //this is what it look slike if you have text in the Widget
         //   child: Text('Hello World!', style: TextStyle(
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-   const StyledText({super.key});
+//this is an option to get dynamic text- not ideal ebcuase it's too complicated
+//    const StyledText(String text, {super.key}): outputText = text;
+
+    StyledText(this.outputText, {super.key});
+
+    final String outputText;
 
     @override
-    Widget build(context) {
-        return const Text('Hello World!', style: TextStyle(
+    Widget build(BuildContext context) {
+        return Text(outputText, style: TextStyle(
             color: Colors.white,
             fontSize: 28.0,
           ), //TextStyle
