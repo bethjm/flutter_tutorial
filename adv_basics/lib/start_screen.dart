424918521635c1653293faa27f7dce7a8a0650e7
bethjm <b.jeanne.mills@gmail.com>
2024-10-17 12:08:55 -0700

    added all stylign to homepage

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -7,7 +7,30 @@ class StartScreen extends StatelessWidget {
     Widget build(context){
         //want to add const becuase it saves memory
         return Center(
-            child: const Text('Start Screen'), //Text
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                Image.asset('assets/images/quiz-logo.png', 
+                width: 300
+                    ), //Image.asset
+                    const SizedBox(height: 80),
+                    const Text(
+                        'Learn Flutter the fun way!', 
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                        ), //TextStyle
+                    ), //Text
+                    const SizedBox(height: 30),
+                    OutlinedButton(
+                        onPressed: () {}, 
+                        style: OutlinedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                        ), //OutlinedButton
+                        child: const Text("Start Quiz"),
+                    ), //OutlinedButton
+                ],
+            ), // Column
         ); //Center
     }
 }
