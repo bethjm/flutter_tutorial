b9fe85a7db4b12dbf70016b86c08a961e1ac76c3
bethjm <b.jeanne.mills@gmail.com>
2024-10-16 17:00:57 -0700

    started quiz app

---
+++ adv_basics/lib/start_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+    StartScreen({super.key});
+
+    @override
+    Widget build(context){
+        //want to add const becuase it saves memory
+        return const Text('Start Screen')
+    }
+}
