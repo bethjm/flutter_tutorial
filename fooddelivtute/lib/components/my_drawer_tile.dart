5880783d4f52bfba6fc1a6ca7249e1a78e247c58
bethjm <b.jeanne.mills@gmail.com>
2024-10-24 09:39:16 -0700

    settings bar

--- fooddelivtute/lib/components/my_drawer_tile.dart
+++ fooddelivtute/lib/components/my_drawer_tile.dart
@@ -14,8 +14,8 @@ class MyDrawerTile extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-        return Padding(
-            padding: const EdgeInsets.all(8.0)
+        return Padding(; 
+            padding: const EdgeInsets.only(left: 25.0)
             child: ListTile(
                 title: Text(
                     text,
--- fooddelivtute/lib/pages/home_page.dart
+++ fooddelivtute/lib/pages/home_page.dart
@@ -12,7 +12,6 @@ class _HomePageState extends State<HomePage> {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-
             appBar: AppBar(title: Text("Home")),
             drawer: MyDrawer(),
         ); //Scaffold
